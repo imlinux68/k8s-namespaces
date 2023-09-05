a5f59d0ffd23141a6ac14c128c2ac448c8f2d73c
imlinux68 <104739714+imlinux68@users.noreply.github.com>
2023-09-05 03:08:28 +0300

    Update delete-ns.sh

--- delete-ns.sh
+++ delete-ns.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 source colors.sh
-
-
-
 function delete_ns(){
+	echo "Current namespaces are: "
 kubectl get namespaces
-read -p "Enter namespace name you wanna delete: " deleteNS
+read -p "Enter a name of namespace you want delete: " deleteNS
+
 kubectl delete namespace ${deleteNS}
 if [[ $? -eq 0 ]]
 	then
-		echo "Namespace at name ${deleteNS} was deleted successfully!!"
+		echo -e "${BGreen}Your namespace ${deleteNS} deleted successfully!!! ${Color_Off}"
 	else
-		echo "You entered a wrong namespace name"
+		echo -e "${Red} You entered a wrong name${Color_Off}"
 fi
-
-
 }
-
-
-
