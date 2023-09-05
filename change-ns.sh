26f10c1ae27d5239cf8c6136e3660624ff6d5dfc
imlinux68 <104739714+imlinux68@users.noreply.github.com>
2023-09-05 03:07:44 +0300

    Update change-ns.sh

--- change-ns.sh
+++ change-ns.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 source colors.sh
 
-
-
-
 function change_ns(){
-echo "All namespaces list:"
+echo "All namespaces list: "
 kubectl get namespaces
-echo "Your current namespace: "
-kubectl config view --minify  | grep namespace
+echo "Your current namespace is: "
+kubectl config view --minify | grep namespace
 
-read -p "Enter a namespace you wanna to change to it: " changeNS
+read -p "Enter a namespace name you want to change to: " changeNS
 kubectl get namespaces | grep -q "${changeNS}"
 if [[ $? -ne 1 ]]
-	then
+	then 
 		kubectl config set-context --current --namespace=${changeNS}
-		echo -e "${BGreen}Your current default namespace is: ${changeNS} ${Color_Off}"
+		echo -e "${BGreen} Your current default namespace is: ${changeNS} ${Color_Off}"	
 	else
-		echo -e "${Red}You entered a wrong name for namespace${Color_Off}"
+		echo -e "${BRed} You entered a wrong namespace name${Color_Off}"
 fi
 
 }
